68b42588e039cc7eccf641d60fce38e6634ffd41
Hsiang-Yu <hsiang-yu.cheng@intel.com>
2019-07-08 15:49:05 -0700

    add cms client code

--- clients.go
+++ clients.go
@@ -1,28 +1,12 @@
 package clients
 
-import (
-	"net/http"
-	"net/url"
-)
+import "net/url"
 
-type BaseHTTPClient struct {
-	BaseURL    string
-	HTTPClient *http.Client
-}
-
-func (c *BaseHTTPClient) ResolvePath(path string) (string, error) {
-	baseURL, err := url.Parse(c.BaseURL)
+func ResolvePath(baseURLIn, path string) (string, error) {
+	baseURL, err := url.Parse(baseURLIn)
 	if err != nil {
 		return "", err
 	}
 	rel := baseURL.ResolveReference(&url.URL{Path: path})
 	return rel.String(), err
-}
-
-func (c *BaseHTTPClient) DoRequest(req *http.Request) (*http.Response, error) {
-
-	if c.HTTPClient == nil {
-		c.HTTPClient = &http.Client{}
-	}
-	return c.HTTPClient.Do(req)
 }
--- cms/cms.go
+++ cms/cms.go
@@ -1,1 +1,66 @@
 package cms
+
+import (
+	"bytes"
+	"errors"
+	"intel/isecl/lib/clients"
+	"net/http"
+)
+
+type CMSClient struct {
+	BaseUrl     string
+	JWTToken    []byte
+	httpClientP *http.Client
+}
+
+var (
+	ErrFailToGetRootCA = errors.New("Failed to retrieve root CA")
+	ErrSignCSRFailed   = errors.New("Failed to sign certificate with CMS")
+)
+
+func (c *CMSClient) httpClient() *http.Client {
+	if c.httpClientP == nil {
+		c.httpClientP = &http.Client{}
+	}
+	return c.httpClientP
+}
+
+func (c *CMSClient) GetRootCA() (string, error) {
+
+	url, err := clients.ResolvePath(c.BaseUrl, "cms/v1/ca-certificates")
+	req, _ := http.NewRequest(http.MethodGet, url, nil)
+	req.Header.Set("Accept", "application/x-pem-file")
+	rsp, err := c.httpClient().Do(req)
+	if err != nil {
+		return "", err
+	}
+	if rsp.StatusCode != http.StatusOK {
+		return "", ErrFailToGetRootCA
+	}
+	resBuf := new(bytes.Buffer)
+	resBuf.ReadFrom(rsp.Body)
+	resStr := resBuf.String()
+	return resStr, nil
+}
+
+func (c *CMSClient) PostCSR(csr []byte) (string, error) {
+
+	url, err := clients.ResolvePath(c.BaseUrl, "cms/v1/certificates")
+	req, _ := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(csr))
+
+	req.Header.Set("Accept", "application/x-pem-file")
+	req.Header.Set("Content-Type", "application/x-pem-file")
+
+	req.Header.Add("Authorization", "Bearer "+string(c.JWTToken))
+	rsp, err := c.httpClient().Do(req)
+	if err != nil {
+		return "", err
+	}
+	if rsp.StatusCode != http.StatusOK {
+		return "", ErrSignCSRFailed
+	}
+	resBuf := new(bytes.Buffer)
+	resBuf.ReadFrom(rsp.Body)
+	resStr := resBuf.String()
+	return resStr, nil
+}
